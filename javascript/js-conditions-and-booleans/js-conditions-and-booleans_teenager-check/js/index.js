711fcf5469a52e50b3289a1ecac1b706c5a42ece
Ruslan Mirasov <info@mirasov.dev>
2024-08-02 15:41:21 +0200

    fix age

--- javascript/js-conditions-and-booleans/js-conditions-and-booleans_teenager-check/js/index.js
+++ javascript/js-conditions-and-booleans/js-conditions-and-booleans_teenager-check/js/index.js
@@ -9,5 +9,7 @@ button.addEventListener("click", () => {
   // Exercise:
   // Use conditions and the logical AND operator to write
   // "You are a teen." or "You are not a teen." into the console.
-  console.log(age < 18 ? "You are a teen." : "You are not a teen.");
+  console.log(
+    age >= 12 && age < 20 ? "You are a teen." : "You are not a teen."
+  );
 });
