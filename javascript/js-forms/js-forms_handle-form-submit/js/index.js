0e6eeff7fd9a7cd2185390211bebc696c0bd0cca
Ruslan Mirasov <info@mirasov.dev>
2024-08-07 13:42:00 +0200

    Handle Form Submit

--- javascript/js-forms/js-forms_handle-form-submit/js/index.js
+++ javascript/js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,30 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const currentForm = e.target;
+  const data = new FormData(currentForm);
+  const formData = {};
+
+  data.forEach((value, key) => {
+    if (value) {
+      formData[key] = value;
+    }
+  });
+
+  console.log(formData);
+
+  const age = Number(formData.age) || 0;
+  const badness = Number(formData.badness);
+  const ageBadnessSum = age + badness;
+
+  console.log(
+    `The age-badness-sum of "${formData.firstName}" is "${ageBadnessSum}"`
+  );
+
+  form.reset();
+  document.getElementById("first-name").focus();
+});
