c25d05cc51cf7d7bc48b186fceaea3d4e01560c2
Ruslan Mirasov <info@mirasov.dev>
2024-08-07 22:14:39 +0200

    Social Media Post is done

--- javascript/js-createElement/js-createelement_social-media-post/js/index.js
+++ javascript/js-createElement/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,36 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+//Creating post element
+const newPost = document.createElement("section");
+newPost.classList.add("post");
+
+//Creating content element
+const newPostContent = document.createElement("p");
+newPostContent.classList.add("post__content");
+newPostContent.textContent = `My new social media post has been successfully published!`;
+
+//Creating footer element
+const newPostFooter = document.createElement("footer");
+newPostFooter.classList.add("post__footer");
+
+//Creating Username element
+const newPostUsername = document.createElement("span");
+newPostUsername.classList.add("post__username");
+newPostUsername.textContent = `@RuslanMirasov`;
+
+//Creating button element
+const newPostButton = document.createElement("button");
+newPostButton.classList.add("post__button");
+newPostButton.setAttribute("type", "button");
+newPostButton.setAttribute("data-js", "like-button");
+newPostButton.textContent = `♥ Like`;
+newPostButton.addEventListener("click", handleLikeButtonClick);
+
+//Merging all elements
+newPostFooter.append(newPostUsername, newPostButton);
+newPost.append(newPostContent, newPostFooter);
+
+//Final render
+document.body.append(newPost);
