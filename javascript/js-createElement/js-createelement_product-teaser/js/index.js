4c1fb0d3079ed8ad31bb66073fcd4dad249a19a4
Ruslan Mirasov <info@mirasov.dev>
2024-08-07 23:33:20 +0200

    Product Teaser is done

--- javascript/js-createElement/js-createelement_product-teaser/js/index.js
+++ javascript/js-createElement/js-createelement_product-teaser/js/index.js
@@ -9,3 +9,44 @@ const category3 = "Plankton Diet";
 const price = "149,99 €";
 const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
+
+const productMarkup = `
+    <article class="product">
+      <div class="product__body">
+        <div class="product__text-container">
+          <h2 class="product__name">${name}</h2>
+          <ul class="product__categories">
+            <li class="product__category">${category1}</li>
+            <li class="product__category">${category2}</li>
+            <li class="product__category">${category3}</li>
+          </ul>
+          <p class="product__description">${description}</p>
+        </div>
+        <div class="product__image-container">
+          <img
+            class="product__image"
+            src="${imageSrc}"
+            alt="${name}"
+          />
+        </div>
+      </div>
+      <footer class="product__footer">
+        <span class="product__price">${price}</span>
+        <button type="button" class="product__buy-button">Buy</button>
+      </footer>
+    </article>
+`;
+
+document.body.innerHTML += productMarkup;
+
+const buyButton = document.querySelectorAll(".product__buy-button");
+
+for (let i = 0; i < buyButton.length; i++) {
+  buyButton[i].addEventListener("click", (e) => {
+    const parentProductEl = e.target.closest(".product");
+    const name = parentProductEl.querySelector(".product__name").innerText;
+    const price = parentProductEl.querySelector(".product__price").innerText;
+    console.log("Name: ", name);
+    console.log("Price: ", price);
+  });
+}
