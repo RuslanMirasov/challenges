38e8e4e17026cf130a8c67fba273da6c541fb560
Ruslan Mirasov <info@mirasov.dev>
2024-08-07 21:36:27 +0200

    Toast message is done

--- javascript/js-createElement/js-createelement_toast-messages/js/index.js
+++ javascript/js-createElement/js-createelement_toast-messages/js/index.js
@@ -4,10 +4,26 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
+function getCorrentTime() {
+  const date = new Date();
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+
+  return `${hours}:${minutes}:${seconds}`;
+}
+
 addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+  const currentTime = getCorrentTime();
+  const newMessageEl = document.createElement("li");
+  newMessageEl.classList.add("toast-container__message");
+  newMessageEl.innerHTML = `
+    <b>I'm a new toast message.</b><br/>
+    Created at <b style="color:#3f0097;">${currentTime}</b>
+  `;
+  toastContainer.append(newMessageEl);
 });
 
 clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
