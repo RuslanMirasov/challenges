615d53a1354ce5ce41a11a8281ec72d3e714c94f
Ruslan Mirasov <info@mirasov.dev>
2024-08-07 11:42:27 +0200

    Multiple Inputs is done

--- javascript/js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
+++ javascript/js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
@@ -7,10 +7,40 @@ Implement the following functionality:
 
 2. On button click: The value of the first input field is copied in uppercase into the second input field
 
-3. On button click: The values of the two input fields switch.
+3. On button click: The values of the two input fields switch. 
 
 */
 
 const firstInput = document.querySelector("[data-js=first-input]");
 const secondInput = document.querySelector("[data-js=second-input]");
 const button = document.querySelector("[data-js=button]");
+
+let fork = 0;
+
+const copyInput = () => {
+  const result = `${secondInput.value} ${firstInput.value}`;
+  secondInput.value = result.trim();
+};
+
+const copyInputUppercase = () => {
+  const result = `${secondInput.value} ${firstInput.value.toUpperCase()}`;
+  secondInput.value = result.trim();
+};
+
+const switchInputs = () => {
+  const firstValue = firstInput.value;
+  const secondValue = secondInput.value;
+  firstInput.value = secondValue;
+  secondInput.value = firstValue;
+};
+
+button.addEventListener("click", () => {
+  fork = fork >= 3 ? 1 : fork + 1;
+  if (fork === 1) {
+    copyInput();
+  } else if (fork === 2) {
+    copyInputUppercase();
+  } else {
+    switchInputs();
+  }
+});
