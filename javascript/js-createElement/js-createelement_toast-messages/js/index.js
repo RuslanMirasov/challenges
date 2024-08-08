9642f35474397e10c01890e8142c9e05a839669d
Ruslan Mirasov <info@mirasov.dev>
2024-08-08 11:57:22 +0200

    refactoring js code

--- javascript/js-createElement/js-createelement_toast-messages/js/index.js
+++ javascript/js-createElement/js-createelement_toast-messages/js/index.js
@@ -4,6 +4,7 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
+// GET Current Time when the masswgw was created
 function getCorrentTime() {
   const date = new Date();
   const hours = date.getHours().toString().padStart(2, "0");
@@ -13,15 +14,23 @@ function getCorrentTime() {
   return `${hours}:${minutes}:${seconds}`;
 }
 
-addButton.addEventListener("click", () => {
+// Creating of new massege markup
+function createNewMassage() {
   const currentTime = getCorrentTime();
   const newMessageEl = document.createElement("li");
+
   newMessageEl.classList.add("toast-container__message");
   newMessageEl.innerHTML = `
     <b>I'm a new toast message.</b><br/>
     Created at <b style="color:#3f0097;">${currentTime}</b>
   `;
-  toastContainer.append(newMessageEl);
+
+  return newMessageEl;
+}
+
+addButton.addEventListener("click", () => {
+  const newMessage = createNewMassage();
+  toastContainer.append(newMessage);
 });
 
 clearButton.addEventListener("click", () => {
