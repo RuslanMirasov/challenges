18dfb6387ed680e17f55adc53bbfce47db69595c
Ruslan Mirasov <info@mirasov.dev>
2024-08-12 16:24:28 +0200

    Stars

--- javascript/js-loops/js-loops_stars/js/index.js
+++ javascript/js-loops/js-loops_stars/js/index.js
@@ -2,13 +2,25 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
+  for (let i = 1; i <= 5; i++) {
+    const src =
+      filledStars >= i ? "./assets/star-filled.svg" : "./assets/star-empty.svg";
 
+    const star = document.createElement("img");
+    star.src = src;
+
+    star.addEventListener("click", () => {
+      renderStars(i);
+    });
+
+    starContainer.append(star);
+  }
   //--^-- your code here --^--
 }
 
-renderStars();
+renderStars(0);
