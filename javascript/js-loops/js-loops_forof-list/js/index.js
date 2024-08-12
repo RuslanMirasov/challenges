57dfc39dc816aa452b7a3fa01dec1f855af69e1b
Ruslan Mirasov <info@mirasov.dev>
2024-08-12 15:36:16 +0200

    List

--- javascript/js-loops/js-loops_forof-list/js/index.js
+++ javascript/js-loops/js-loops_forof-list/js/index.js
@@ -18,5 +18,7 @@ const programmingLanguages = [
 ];
 
 // --v-- write/change code here --v--
-
+for (const language of programmingLanguages) {
+  ol.innerHTML += `<li>${language}</li>`;
+}
 // --^-- write/change code here --^--
