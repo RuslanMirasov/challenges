54eab6f070e5775138453ad60e5572484877c548
Ruslan Mirasov <info@mirasov.dev>
2024-08-15 01:02:34 +0200

    Sleep is done

--- javascript/js-async-functions/js-async-functions_sleep/js/index.js
+++ javascript/js-async-functions/js-async-functions_sleep/js/index.js
@@ -8,17 +8,19 @@ const body = document.body;
 
 button3.addEventListener("click", async () => {
   icon.textContent = "💤";
-  sleep(3);
+  await sleep(3);
   icon.textContent = "⏰";
 });
 
-button5.addEventListener("click", () => {
+button5.addEventListener("click", async () => {
   icon.textContent = "💤";
+  await sleep(5);
   icon.textContent = "⏰";
 });
 
-function handleSleepFor10Button() {
+async function handleSleepFor10Button() {
   icon.textContent = "💤";
+  await sleep(10);
   icon.textContent = "⏰";
 }
 
@@ -28,8 +30,12 @@ button10.addEventListener("click", handleSleepFor10Button);
 
 // returns a Promise that resolves after x seconds
 function sleep(seconds) {
+  document.body.classList.add("dark");
   const sleepPromise = new Promise((resolve) => {
-    setTimeout(() => resolve(), seconds * 1000);
+    setTimeout(() => {
+      document.body.classList.remove("dark");
+      resolve();
+    }, seconds * 1000);
   });
   return sleepPromise;
 }
