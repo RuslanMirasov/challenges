272e9f11a4776c37af4d6497fa47a56e2fefcbde
Ruslan Mirasov <info@mirasov.dev>
2024-08-19 17:55:53 +0200

    Retro Code is done

--- javascript/js-modern-syntax/js-modern-syntax_retro-coding/index.js
+++ javascript/js-modern-syntax/js-modern-syntax_retro-coding/index.js
@@ -1,12 +1,39 @@
-let getNameAndCountry = ({ name, country }) => [name, country];
+// let getNameAndCountry = ({ name, country }) => [name, country];
 
-let getRelocatedCity = (
-  city1,
-  city2 = { name: "Berlin", country: "Germany" }
-) => {
-  let [, country] = getNameAndCountry(city2);
-  return {
-    ...city1,
-    country,
-  };
-};
+// let getRelocatedCity = (
+//   city1,
+//   city2 = { name: "Berlin", country: "Germany" }
+// ) => {
+//   let [, country] = getNameAndCountry(city2);
+//   return {
+//     ...city1,
+//     country,
+//   };
+// };
+
+function getNameAndCountry(city) {
+  const nameAndCountry = [city.name, city.country];
+  return nameAndCountry;
+}
+
+function getRelocatedCity(city1, city2) {
+  if (!city2) {
+    city2 = { name: "Berlin", country: "Germany" };
+  }
+
+  const nameAndCountry = getNameAndCountry(city2);
+  const country = nameAndCountry[1];
+
+  const relocatedCity = {};
+  for (const key in city1) {
+    relocatedCity[key] = city1[key];
+  }
+  relocatedCity.country = country;
+
+  return relocatedCity;
+}
+
+var city1 = { name: "Mariupol", country: "Ukraine" };
+var city2 = { name: "Berlin", country: "Germany" };
+
+console.log(getRelocatedCity(city1, city2));
