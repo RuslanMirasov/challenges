c62ede302fa8b98be365ec90126d3d6766a1b4f1
Ruslan Mirasov <info@mirasov.dev>
2024-08-20 13:28:04 +0200

    Get Animals is done

--- tests/js-unit-testing_get-animal/index.js
+++ tests/js-unit-testing_get-animal/index.js
@@ -1,1 +1,7 @@
-export function getAnimal() {}
+export function getAnimal(animals) {
+  if (!animals) {
+    return `I do not like animals at all!`;
+  }
+
+  return animals === "cats" ? `I totally love cats!` : `I like ${animals}!`;
+}
