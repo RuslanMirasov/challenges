9391697996e430a39bb49abac06327444a278520
Ruslan Mirasov <info@mirasov.dev>
2024-08-23 15:42:46 +0200

    Attributes is done

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,18 @@
 import "./App.css";
 
+const Article = () => {
+  return (
+    <article className="article">
+      <h2 className="article__title">Hello World</h2>
+      <label htmlFor="name">My name</label>
+      <input type="text" id="name" name="name" placeholder="Enter your name" />
+      <a href="https://www.wikipedia.org/" className="article__link">
+        Wikipedia
+      </a>
+    </article>
+  );
+};
+
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
 }
