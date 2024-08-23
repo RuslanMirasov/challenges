dee10331a850c77ed11593a15827d1b74244632a
Ruslan Mirasov <info@mirasov.dev>
2024-08-23 22:53:29 +0200

    download new challenges

---
+++ react-basics/Journal-App/journal/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p className="footer__copyright">&copy;2024 Journal app copyright.</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ react-basics/Journal-App/journal/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="header__title">Journal</h1>
+    </div>
+  );
+};
+
+export default Header;
