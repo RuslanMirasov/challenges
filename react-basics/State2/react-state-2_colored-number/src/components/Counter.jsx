24129dc0b383828411fa6d8721799076b347c3dc
Ruslan Mirasov <info@mirasov.dev>
2024-08-28 15:36:40 +0200

    Colored Number is done

--- react-basics/State2/react-state-2_colored-number/src/components/Counter.jsx
+++ react-basics/State2/react-state-2_colored-number/src/components/Counter.jsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import ColoredNumber from "./ColoredNumber";
 import "./Counter.css";
 
-
 export default function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrementCounter = () => {
+    setCounter(counter - 1);
+  };
+
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      <ColoredNumber value={counter} />
       <div className="counter__buttons-container">
         <button
           type="button"
           className="counter__button"
           aria-label="increment count"
+          onClick={incrementCounter}
         >
           +
         </button>
@@ -19,6 +30,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement count"
+          onClick={decrementCounter}
         >
           -
         </button>
