564264a64fb7ba2d32734f254bae00cf97fff5b0
Ruslan Mirasov <info@mirasov.dev>
2024-08-28 16:21:04 +0200

    personal-details-form is done

--- react-basics/State2/react-state-2_personal-details/src/components/Form.jsx
+++ react-basics/State2/react-state-2_personal-details/src/components/Form.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
 import "./Form.css";
 
-
-export default function Form() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-
+export default function Form({ onCreateUser }) {
   function handleSubmit(event) {
     event.preventDefault();
+    const form = event.target;
+    const formData = Object.fromEntries(new FormData(form).entries());
+    onCreateUser(formData.name, formData.email);
+    form.reset();
   }
 
   return (
