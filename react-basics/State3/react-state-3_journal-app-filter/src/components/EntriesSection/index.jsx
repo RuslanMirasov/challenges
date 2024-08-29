a0e112d6b6ba740184fe3664a0ac665a0fab39c7
Ruslan Mirasov <info@mirasov.dev>
2024-08-29 12:34:07 +0200

    Filter is done

--- react-basics/State3/react-state-3_journal-app-filter/src/components/EntriesSection/index.jsx
+++ react-basics/State3/react-state-3_journal-app-filter/src/components/EntriesSection/index.jsx
@@ -6,15 +6,30 @@ import Tab from "../Tab";
 import Badge from "../Badge";
 import { Fragment } from "react";
 
-export default function EntriesSection({ entries, onToggleFavorite }) {
+export default function EntriesSection({
+  filter,
+  entries,
+  onToggleFavorite,
+  onShowAllEntries,
+  onShowFavoriteEntries,
+  allEntriesCount,
+  favoriteEntriesCount,
+}) {
   return (
     <section className="entries-section">
       <Tabs>
-        <Tab active>
-          All Entries <Badge isActive>3</Badge>
+        <Tab isActive={filter === "all" && true} onClick={onShowAllEntries}>
+          All Entries{" "}
+          <Badge isActive={filter === "all" && true}>{allEntriesCount}</Badge>
         </Tab>
-        <Tab>
-          Favorites <Badge>1</Badge>
+        <Tab
+          isActive={filter !== "all" && true}
+          onClick={onShowFavoriteEntries}
+        >
+          Favorites{" "}
+          <Badge isActive={filter !== "all" && true}>
+            {favoriteEntriesCount}
+          </Badge>
         </Tab>
       </Tabs>
       <div className="entries-section__entries">
