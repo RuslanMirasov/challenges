57b591e68d1ad305c2172f4e02ed5830688dddec
Ruslan Mirasov <info@mirasov.dev>
2024-08-30 15:54:04 +0200

    Dice is done

--- react-basics/React-with-Local-Storage/react-with-local-storage_dice/src/App.jsx
+++ react-basics/React-with-Local-Storage/react-with-local-storage_dice/src/App.jsx
@@ -1,15 +1,30 @@
+import useLocalStorageState from "use-local-storage-state";
+import { getD6Roll } from "./utils";
 import { D6Button } from "./components/D6Button";
 import History from "./components/History";
 import "./App.css";
+import { useEffect } from "react";
 
 export default function App() {
+  const [rolls, setRolls] = useLocalStorageState("rolls", { defaultValue: [] });
+
+  const handleRoll = () => {
+    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+  };
+
+  const currentRollValue = rolls[0]?.value;
+
+  useEffect(() => {
+    console.log(rolls);
+  }, [rolls]);
+
   return (
     <div className="app">
       <main className="app__main">
-        <D6Button />
+        <D6Button value={currentRollValue} onRoll={handleRoll} />
       </main>
       <aside className="app__aside">
-        <History rolls={[]} />
+        <History rolls={rolls} />
       </aside>
     </div>
   );
