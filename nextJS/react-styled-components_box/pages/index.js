719770721122ac632f75ebf9e58e4b95b6d1fcd7
Ruslan Mirasov <info@mirasov.dev>
2024-09-06 11:07:24 +0200

    box with styles components is done

--- nextJS/react-styled-components_box/pages/index.js
+++ nextJS/react-styled-components_box/pages/index.js
@@ -1,3 +1,4 @@
+import StyledBox from "@/components/BoxWithStyledComponents.js";
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
 
 export default function HomePage() {
@@ -5,6 +6,8 @@ export default function HomePage() {
     <div>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
+      <StyledBox />
+      <StyledBox $isBlack />
     </div>
   );
 }
