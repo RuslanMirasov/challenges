47d671383a0537658833a191ca965b8e50c60a4d
Ruslan Mirasov <info@mirasov.dev>
2024-09-06 11:09:35 +0200

    delete challenge

--- nextJS/react-styled-components_box/pages/index.js
+++ nextJS/react-styled-components_box/pages/index.js
@@ -6,8 +6,6 @@ export default function HomePage() {
     <div>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
-      <StyledBox />
-      <StyledBox $isBlack />
     </div>
   );
 }
