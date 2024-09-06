fbaea3cb7dc0ced02a946eb419d37005ec1fa941
Ruslan Mirasov <info@mirasov.dev>
2024-09-06 11:11:37 +0200

    styled box challenge is done

--- nextJS/react-styled-components_box/pages/index.js
+++ nextJS/react-styled-components_box/pages/index.js
@@ -6,6 +6,8 @@ export default function HomePage() {
     <div>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
+      <StyledBox />
+      <StyledBox $isBlack />
     </div>
   );
 }
