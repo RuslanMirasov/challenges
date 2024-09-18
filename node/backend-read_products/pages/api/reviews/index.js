59669ac1f7f66af673e1aa6b8f6ac2a3915b0b6d
Ruslan <info@mirasov.dev>
2024-09-18 14:43:58 +0200

    Backend Read (Fish Shop)

--- node/backend-read_products/pages/api/reviews/index.js
+++ node/backend-read_products/pages/api/reviews/index.js
@@ -1,10 +1,10 @@
 import dbConnect from "@/db/connect";
-import Product from "@/db/models/Product";
+import Review from "@/db/models/Review";
 
 export default async function handler(request, response) {
   await dbConnect();
   if (request.method === "GET") {
-    const products = await Product.find();
-    response.status(200).json(products);
+    const reviews = await Review.find();
+    response.status(200).json(reviews);
   }
 }
