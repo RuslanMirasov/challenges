f136781063818c1734a594c10692cba36d0afaf3
Ruslan <info@mirasov.dev>
2024-09-18 15:58:50 +0200

    Create a product

--- node/backend-create_products/pages/api/products/index.js
+++ node/backend-create_products/pages/api/products/index.js
@@ -4,8 +4,21 @@ import Product from "../../../db/models/Product";
 export default async function handler(request, response) {
   await dbConnect();
 
+  //  ---GET---  OLL PRODUCTS
   if (request.method === "GET") {
     const products = await Product.find();
     return response.status(200).json(products);
+  }
+
+  //  ---POST--- A NEW PRODUCT
+  if (request.method === "POST") {
+    try {
+      const productData = request.body;
+      await Product.create(productData);
+      return response.status(201).json({ status: "Product created." });
+    } catch (error) {
+      console.error("Error creating product:", error);
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
